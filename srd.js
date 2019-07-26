0966cd55f7ad550cfd79488598e1f7b679f36c1e
imagineSolo <48104451+imagineSolo@users.noreply.github.com>
2019-07-26 22:48:07 +0200

    Delete srd.js

--- srd.js
+++
@@ -1,9 +0,0 @@
-class SRD {
-  async getSpells(optionSelected) {
-    const dataResponse = await fetch('spells.json');
-    const data = await dataResponse.json();
-    ui.classSpells(optionSelected, data);
-    ui.levelSpells(optionSelected, data);
-    ui.schoolSpells(optionSelected, data);
-  }
-}
