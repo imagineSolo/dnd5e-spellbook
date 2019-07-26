e7c8ff326e70e3a7f54bf9038cc094a3711ba079
imagineSolo <48104451+imagineSolo@users.noreply.github.com>
2019-07-26 22:47:25 +0200

    Delete app.js

--- Spellbook/app.js
+++
@@ -1,6 +0,0 @@
-const testOutput = document.querySelector('#test-output');
-
-document.querySelector('#class-select').addEventListener('change', function (e) {
-  console.log(this.value);
-  testOutput.textContent = e.target.value;
-});
